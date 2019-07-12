02665d55f96e4feeda31855ca2e15c87aa26a4c3
klequis <klequis@gmail.com>
2019-07-12 06:39:45 -0700

    unexplained error

--- src/config/index.js
+++ src/config/index.js
@@ -2,12 +2,10 @@ const domain = 'klequis-todo.auth0.com'
 
 const config = {
   auth0: {
-    // domain: domain,
-    // audience: `https://${domain}/userinfo/`,
-    // clientId: 'Hav4pitWXpDGkMaAxpj7rxHYuwAZovyI',
-    domain: "klequis-todo.auth0.com",
-    clientId: "Hav4pitWXpDGkMaAxpj7rxHYuwAZovyI",
-    audience: "https://klequis-todo.tk",
+    // domain: 'klequis-todo.auth0.com',
+    domain: domain,
+    audience: 'https://klequis-todo.tk',
+    clientId: 'Hav4pitWXpDGkMaAxpj7rxHYuwAZovyI',
     redirectUri: 'http://localhost:3000/',
     responseType: 'id_token',
     scope: 'openid profile'
@@ -15,3 +13,22 @@ const config = {
 }
 
 export default config
+
+
+// const domain = 'klequis-todo.auth0.com'
+
+// const config = {
+//   auth0: {
+//     // domain: domain,
+//     // audience: `https://${domain}/userinfo/`,
+//     // clientId: 'Hav4pitWXpDGkMaAxpj7rxHYuwAZovyI',
+//     domain: "klequis-todo.auth0.com",
+//     clientId: "Hav4pitWXpDGkMaAxpj7rxHYuwAZovyI",
+//     audience: "https://klequis-todo.tk",
+//     redirectUri: 'http://localhost:3000/',
+//     responseType: 'id_token',
+//     scope: 'openid profile'
+//   }
+// }
+
+// export default config
