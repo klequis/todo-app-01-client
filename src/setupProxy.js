663f4acd1777bdf9bf81b99d605a4e055d4eaf57
klequis <klequis@gmail.com>
2019-07-16 10:05:00 -0700

    switch proxy based on environment

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,5 +1,15 @@
 const proxy = require('http-proxy-middleware')
 
+console.log('process.env.NODE_ENV', process.env.NODE_ENV)
+
 module.exports = function(app) {
-  app.use(proxy('/api', { target: 'http://localhost:3030/' }))
+  const env = process.env.NODE_ENV
+  if (!env === 'production') {
+    app.use(proxy('/api', { target: 'http://localhost:3030/' }))
+  } else {
+    app.use(proxy('/api', { 
+      target: 'https://api.klequis-todo.tk/',
+      changeOrigin: true,
+    }))
+  }
 }
