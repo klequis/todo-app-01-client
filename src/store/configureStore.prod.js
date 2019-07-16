b6f40721b864a13827a7a836ba2f5fb5c69433ee
klequis <klequis@gmail.com>
2019-07-16 10:05:29 -0700

    Sync *.prod versions of files with *.dev version

--- src/store/configureStore.prod.js
+++ src/store/configureStore.prod.js
@@ -1,12 +1,11 @@
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
-import api from '../middleware/api'
-import rootReducer from '../reducers'
+import rootReducer from './root-reducer'
 
 const configureStore = preloadedState => createStore(
   rootReducer,
   preloadedState,
-  applyMiddleware(thunk, api)
+  applyMiddleware(thunk)
 )
 
 export default configureStore
