c26b581a1dc8fa68b7e040982a3e28a0f3e53a08
klequis <klequis@gmail.com>
2019-09-01 09:15:13 -0700

    add userReducer

--- src/store/root-reducer.js
+++ src/store/root-reducer.js
@@ -3,13 +3,15 @@ import { todosReducer } from './todo/reducers'
 import { requestsReducer } from './requests/reducers'
 import { toastReducer } from './toast/reducers'
 import { validationErrorsReducer } from './validation/reducers'
+import { userReducer } from './user/reducers'
 
 
 const rootReducer = combineReducers({
   todos: todosReducer,
   requests: requestsReducer,
   toast: toastReducer,
-  validationErrors: validationErrorsReducer
+  validationErrors: validationErrorsReducer,
+  userId: userReducer
 })
 
 export default rootReducer
