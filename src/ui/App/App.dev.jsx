d5997b78d177d009e6fc5e83619061f908e94126
klequis <klequis@gmail.com>
2019-09-03 17:09:25 -0700

    stop using route for now

--- src/ui/App/App.dev.jsx
+++ src/ui/App/App.dev.jsx
@@ -1,45 +1,51 @@
 import React from 'react'
-import {
-  Link,
-  Switch,
-  Route,
-  BrowserRouter as Router /*, Link */
-} from 'react-router-dom'
-import NavBar from 'ui/NavBar'
-import PrivateRoute from 'elements/PrivateRoute'
+// import {
+  // Link,
+  // Switch,
+  // Route,
+  // BrowserRouter as Router /*, Link */
+// } from 'react-router-dom'
+// import NavBar from 'ui/NavBar'
+// import PrivateRoute from 'elements/PrivateRoute'
 import Todos from 'ui/Todos'
 import Status from 'ui/Status'
-import Home from 'ui/Home'
+import Login from 'ui/Login'
 import DevTools from 'ui/DevTools'
 import withStyles from 'react-jss'
 import { useAuth0 } from 'react-auth0-spa'
 import Toasts from 'ui/Toasts'
 
 function App({ classes }) {
-  const { loading } = useAuth0()
+  const { loading, isAuthenticated } = useAuth0()
   if (loading) {
     return <h1>Loading</h1>
-  } else {
-    return (
-      <>
-        <Toasts />
-        <div className={classes.wrapper}>
-        <Router>
-          <Status />
-          <NavBar />
-          <div>
-            <Link to="/todos">Todos</Link>
-          </div>
-          <Switch>
-            <PrivateRoute exact path="/todos" component={Todos} />
-            <Route exact path="/" component={Home} />
-          </Switch>
-        </Router>
-      <DevTools />
-    </div>
-    </>
-    )
   }
+
+  return (
+    // <Router>
+      <div className={classes.wrapper}>
+        <Toasts />
+        <Login />
+        <Status />
+        {/* <Switch> */}
+          {/* <Route
+            path="/"
+            render={() =>
+              isAuthenticated 
+                ? <Todos />
+                : null
+            }
+          /> */}
+          {
+            isAuthenticated
+              ? <Todos />
+              : null
+          }
+        {/* </Switch> */}
+        <DevTools />
+      </div>
+    // </Router>
+  )
 }
 
 const styles = {
