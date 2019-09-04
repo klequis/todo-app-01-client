d28e112de4b5646b74a8fd34ad9e2b551c95488f
klequis <klequis@gmail.com>
2019-09-03 17:09:00 -0700

    remove logging

--- src/store/validation/reducers.js
+++ src/store/validation/reducers.js
@@ -5,16 +5,16 @@ import { blue } from 'logger'
 export function validationErrorsReducer(state = [], action) {
   switch (action.type) {
     case CLEAR_VALIDATION_ERRORS:
-      blue(`action`, action)
+      // blue(`action`, action)
       // return merge(state, {
       //   [action.requestKey]: { status: REQUEST_PENDING, error: null }
       // })
       // TODO: does not seem this would clearn the errors. Maybe just return [] always
       return state
     case SET_VALIDATION_ERRORS:
-      blue(`action`, action)
-      blue('validation: state', state)
-      blue('validation: payload.errors', action.payload.errors)
+      // blue(`action`, action)
+      // blue('validation: state', state)
+      // blue('validation: payload.errors', action.payload.errors)
       // return merge(state, {
       //   [action.requestKey]: { status: REQUEST_SUCCESS, error: null }
       // })
