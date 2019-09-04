085b1896eb8a82924e758486b25c2bf79698be21
klequis <klequis@gmail.com>
2019-09-03 17:07:53 -0700

    remove logging

--- src/store/toast/reducers.js
+++ src/store/toast/reducers.js
@@ -1,10 +1,11 @@
 import { SET_TOAST, CLEAR_TOAST } from './constants'
-
+import { blue } from 'logger'
 export const toastReducer = (state = null, { type, payload }) => {
   switch (type) {
     case SET_TOAST:
       return payload
     case CLEAR_TOAST:
+      blue('clear toast called')
       return null
     default:
       return state
